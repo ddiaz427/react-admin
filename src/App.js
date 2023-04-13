d78e4f5ca8470cabd017b9f2d1d5ccf575ea15ad
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2023-04-13 14:58:15 -0500

    Create the Sidebar and fix some issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import Dashboard from "./scenes/dashboard";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [theme, colorMode] = useMode()
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
