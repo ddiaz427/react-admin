9e8034e17bcd52796cdb4090dc2059244d31f3e8
Dario Alexander Diaz Florez <ingdariodiaz@gmail.com>
2023-04-14 11:41:44 -0500

    Create Form and Invoices pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import Contacts from "./scenes/contacts";
 import Dashboard from "./scenes/dashboard";
+import Form from "./scenes/form";
+import Invoices from "./scenes/invoices";
 import Team from "./scenes/team";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
@@ -21,6 +23,8 @@ function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
             </Routes>
           </main>
         </div>
